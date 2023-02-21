0bc1f33a8fad6d694c1538bfedd45f45d1af498a
Thomas Stevens <thomas@bbd.co.za>
2023-02-21 20:29:45 +0200

    Finished store procedure. Joins need to be cleaned up, but it works

--- stored_proc.sql
+++ stored_proc.sql
@@ -2,16 +2,24 @@ use RoomBookingDB;
 GO
 
 CREATE PROCEDURE [dbo].[sp_GetBookingsInRange]
-@StartDate datetime = '2023-02-22T00:00:00',
-@EndDate datetime = '2023-02-23T23:59:00',
-@RoomType int = NULL,
-@Office int = NULL
+@StartDate datetime = '2023-02-23T00:00:00',
+@EndDate datetime   = '2023-03-23T11:00:00',
+@RoomType int = 1,
+@Office int = 1
 AS
 BEGIN
-SELECT dbo.bookings.booking_id, dbo.bookings.description, dbo.rooms.room_name
+SELECT dbo.bookings.booking_id, dbo.staff.first_name, dbo.staff.last_name, dbo.bookings.description, dbo.bookings.start_time, dbo.rooms.room_name, dbo.room_types.type, dbo.offices.city
 FROM dbo.bookings FULL JOIN dbo.rooms
 ON dbo.bookings.room_id = dbo.rooms.room_id
+FULL JOIN dbo.room_types
+ON dbo.room_types.room_type_id = dbo.room_types.room_type_id
+FULL JOIN dbo.offices
+ON dbo.rooms.office_id = dbo.offices.office_id
+FULL JOIN dbo.staff
+ON dbo.bookings.staff_id = dbo.staff.staff_id
 WHERE dbo.bookings.start_time BETWEEN @StartDate AND @EndDate
+AND dbo.room_types.room_type_id = @RoomType
+AND dbo.offices.office_id = @Office
 
 END
 GO
