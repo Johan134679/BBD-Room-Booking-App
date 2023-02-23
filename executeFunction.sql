f55ca935db4d1fe6095092a65e06bc32af25eb71
Thomas Stevens <thomas@bbd.co.za>
2023-02-23 14:28:15 +0200

    fixed function to work with new date

--- executeFunction.sql
+++ executeFunction.sql
@@ -1,4 +1,4 @@
 use RoomBookingDB;
 GO
 
-SELECT * FROM [dbo].[fu_GetBookingsInRange]('2023-02-21T01:00:00', '2023-04-23T00:00:00', 'meeting room', 'Johannesburg')
+SELECT * FROM [dbo].[fu_GetBookingsInRange]('2023-02-21', '01:00:00', '2023-04-23', '10:00:00', 'conference room', 'Johannesburg')
--- function.sql
+++ function.sql
@@ -3,14 +3,16 @@ GO
 DROP FUNCTION IF EXISTS [dbo].[fu_GetBookingsInRange] 
 GO
 CREATE FUNCTION [dbo].[fu_GetBookingsInRange](
-@StartDate datetime = '2023-02-23T00:00:00',
-@EndDate datetime   = '2023-03-23T11:00:00',
+@StartDate date = '2023-02-23',
+@StartTime time = '00:00:00',
+@EndDate date   = '2023-03-23',
+@EndTime time = '11:00:00',
 @RoomType varchar(40) = 'Meeting',
 @City varchar(85) = 'Johannesburg'
 )
 RETURNS TABLE
 AS RETURN
-SELECT b.booking_id, s.first_name, s.last_name, b.description, b.start_time, r.room_name, t.type, o.city
+SELECT b.booking_id, s.first_name, s.last_name, b.description, b.start_date, b.start_time, r.room_name, t.type, o.city
 FROM dbo.bookings AS b 
 LEFT JOIN dbo.rooms AS r
 ON b.room_id = r.room_id
@@ -21,9 +23,10 @@ ON r.office_id = o.office_id
 left JOIN dbo.staff as s
 ON b.staff_id = s.staff_id
 WHERE 
-(b.start_time BETWEEN @StartDate AND @EndDate)
+(b.start_date) BETWEEN @StartDate AND @EndDate
+AND (b.start_time BETWEEN @StartTime AND @EndTime)
 AND UPPER(t.type) = UPPER(@RoomType)
-AND (UPPER(o.city) = UPPER(@City)	)
+AND (UPPER(o.city) = UPPER(@City))
 GO
 
 -- 2023-02-22T00:00:00
